cfe644f4808c8e3a49a3ddcd9402b6db14b22a33
YousefMohsen <yousinho7@live.dk>
2017-09-07 10:11:18 +0200

    expressJS rest set up

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('in path user ');
+  console.log("prinbting console");
+});
+
+router.get('/1', function(req, res, next) {
+  res.send('Yousef');
+});
+router.get('/2', function(req, res, next) {
+  res.send('Janus');
+});
+module.exports = router;
