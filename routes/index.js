0279d66e81e3a28e457e1c2de4229d0afd40b059
YousefMohsen <yousinho7@live.dk>
2017-09-15 20:53:07 +0200

    Sample json added for client development

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var messages = [
+  { title: "Lektier til imorgen", msg: "Til imorgen skal i spise 4 flødeboller fra netto samt 1/2 liter mælk", sender: "Leo Messi", date: new Date(2017,09,02,12,02,0,0)},
+  { title: "Lektier til på tirsdag", msg: "Læs bogen Sapien - en kort historie om menneskeheden ", sender: "Thiago", date: new Date(2017,09,13,12,02,0,0)},
+  { title: "Aflyst time", msg: "Barcelona spiller imorgen og timen er defor selvfølgelig aflyst", sender: "Xavi", date: new Date(2017,09,02,14,02)}
+  
 
+];
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'LektApp' });
 
-res.send("Welcome to lektApp, Kasper");
+res.json(messages);
 });
 
 module.exports = router;
--- routes/message.js
+++ routes/message.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('in path user ');
+  res.send('in path message ');
   console.log("prinbting console");
 });
 
